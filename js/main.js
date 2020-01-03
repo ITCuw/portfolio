325ab9fc6e307a0e6021852755ace2ffae8237cb
ITCuw <alexdc2@uw.edu>
2020-01-03 13:00:08 -0800

    add slides for radar project and content for about section

--- js/main.js
+++ js/main.js
@@ -1,26 +1,37 @@
 //Get Modal Elements
 var modal = document.getElementById('modal');
 //Get opem Modal botton
-var modalBtn = document.getElementById('readMore')
+var openBtn = document.querySelectorAll('.openBtn.btn');
+var allSlides = document.getElementsByClassName("slide");
 //Get Close Button
-var closeBtn = document.getElementsByClassName('closeBtn')[0];
-//Listen for Open Click Event
-modalBtn.addEventListener('click', openModal);
-//Listen for Close Click Event
-closeBtn.addEventListener('click', closeModal);
-//Listen For Outside Click
-window.addEventListener('click', clickOutside);
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
 //Open Modal
-function openModal(){
-  modal.style.display = 'flex';
-}
+[].forEach.call(openBtn, function(el) {
+  el.onclick = function() {
+      if (el.id == 'vimocityOpen'){
+        //uncheck all slides
+        allSlides.checked = false;
+        //check intro Vimocity Slide
+        document.getElementById("i1").checked = true;
+        //display content
+        modal.style.display = "flex";
+      } else if (el.id == 'radarOpen'){
+        //uncheck all slides
+        allSlides.checked = false;
+        //check intro Vimocity Slide
+        document.getElementById("i6").checked = true;
+        //display content
+        modal.style.display = "flex";
+    }
+  }
+})
 //Close Modal
-function closeModal(){
+closeBtn.onclick = function(){
   modal.style.display = 'none';
 }
 //Close Modal if Outside Click
-function clickOutside(e){
-  if(e.target == modal){
-    modal.style.display = 'none';
-  }
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
